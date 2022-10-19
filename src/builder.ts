406a0cdb05a4df5de16fdcf166c81a3e77fddb29
a-alhm <a.alhmoud@outlook.com>
2022-10-19 18:23:33 +0300

    Remove underscore from property names

--- src/builder.ts
+++ src/builder.ts
@@ -4,66 +4,66 @@ import { injectable } from "inversify";
 
 @injectable()
 export default class PNGBuilder {
-  private _png: PNG;
-  private _InflatedImageData: Array<number>;
+  private png: PNG;
+  private InflatedImageData: Array<number>;
 
   constructor() {
-    this._png = new PNG();
-    this._InflatedImageData = [];
+    this.png = new PNG();
+    this.InflatedImageData = [];
   }
 
   setWidth(width: number): PNGBuilder {
-    this._png.width = width;
+    this.png.width = width;
     return this;
   }
   setHeight(height: number): PNGBuilder {
-    this._png.height = height;
+    this.png.height = height;
     return this;
   }
   setBitDepth(bitDepth: number): PNGBuilder {
-    this._png.bitDepth = bitDepth;
+    this.png.bitDepth = bitDepth;
     return this;
   }
   setColor(color: number): PNGBuilder {
-    this._png.color = color;
+    this.png.color = color;
     return this;
   }
   setCompressionMethod(method: number): PNGBuilder {
-    this._png.compressionMethod = method;
+    this.png.compressionMethod = method;
     return this;
   }
   setFilterMethod(method: number): PNGBuilder {
-    this._png.filterMethod = method;
+    this.png.filterMethod = method;
     return this;
   }
   setPaletteEntries(entries: Uint8Array[]): PNGBuilder {
-    this._png.paletteEntries = entries;
+    this.png.paletteEntries = entries;
     return this;
   }
   setPaletteEntriesTransparency(entries: Uint8Array): PNGBuilder {
-    this._png.paletteEntriesTransparency = entries;
+    this.png.paletteEntriesTransparency = entries;
     return this;
   }
   setIsInterlaced(isInterlaced: number): PNGBuilder {
-    this._png.isInterlaced = isInterlaced;
+    this.png.isInterlaced = isInterlaced;
     return this;
   }
   pushImageData(data: Uint8Array): PNGBuilder {
-    this._InflatedImageData.push(...data);
+    this.InflatedImageData.push(...data);
     return this;
   }
   inflateImageData(): PNGBuilder {
-    this._png.ImageData = pako.inflate(this._InflatedImageData);
+    this.png.ImageData = pako.inflate(this.InflatedImageData);
     return this;
   }
   setGamaIntensity(data: number): PNGBuilder {
-    this._png.gamaIntensity = data;
+    this.png.gamaIntensity = data;
     return this;
   }
   setChromaticities(data: number[][]) {
-    this._png.chromaticities = data;
+    this.png.chromaticities = data;
   }
   getPNG(): PNG {
-    return this._png;
+    return this.png;
   }
 }
