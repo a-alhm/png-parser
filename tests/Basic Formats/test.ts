b21777aef3d2779cd0b528a93c16e7a10fe6f832
a-alhm <a.alhmoud@outlook.com>
2022-10-30 10:32:12 +0300

    Add testing for all properties

--- tests/Basic Formats/test.ts
+++ tests/Basic Formats/test.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 import { expect } from "chai";
 import parsePng from "../../index";
-import PNG from "../../src/png";
+import { PNG } from "../../src/png";
 
 describe("Basic Formats", () => {
   it("black & white", () => {
@@ -10,7 +10,19 @@ describe("Basic Formats", () => {
       "data",
       (data) => {
         const png = parsePng(data);
-        expect(png).instanceOf(PNG);
+        expect(png.suggestedPalette).with.lengthOf(0);
+        expect(png.textualData).with.lengthOf(0);
+        expect(png.compressedTextualData).with.lengthOf(0);
+        expect(png.internationalTextualData).with.lengthOf(0);
+        expect(png.width).to.equal(32);
+        expect(png.height).to.equal(32);
+        expect(png.bitDepth).to.equal(1);
+        expect(png.color).to.equal(0);
+        expect(png.compressionMethod).to.equal(0);
+        expect(png.filterMethod).to.equal(0);
+        expect(png.isInterlaced).to.equal(0);
+        expect(png.gamaIntensity).to.equal(100000);
+        expect(png.imageData).with.lengthOf(160);
       }
     );
   });
