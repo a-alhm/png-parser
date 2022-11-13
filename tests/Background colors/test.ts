9b74d39a9b037f9d65b753c8dbd8684ae061ebf7
a-alhm <a.alhmoud@outlook.com>
2022-11-13 18:57:38 +0300

    Add Background colors tests

--- tests/Background colors/test.ts
+++ tests/Background colors/test.ts
@@ -1,7 +1,7 @@
 import path from "path";
 import PNGTest from "../PNGTest";
 
-describe("Basic Formats", () => {
+describe("Background colors", () => {
   describe("bits rgb color, alpha, white background chunk", () => {
     const test = new PNGTest();
 
@@ -11,13 +11,13 @@ describe("Basic Formats", () => {
     test.internationalTextualDataLength = 0;
     test.width = 32;
     test.height = 32;
-    test.bitDepth = 1;
-    test.color = 0;
+    test.bitDepth = 8;
+    test.color = 6;
     test.compressionMethod = 0;
     test.filterMethod = 0;
     test.isInterlaced = 0;
     test.gamaIntensity = 100000;
-    test.imageDataLength = 160;
+    test.imageDataLength = 4128;
 
     test.parsePng(path.join(__dirname, "images", "bgwn6a08.png"), test);
   });
