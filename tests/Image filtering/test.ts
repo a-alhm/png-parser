ca802007db53167fd42779239d03f169dd7dcd39
a-alhm <a.alhmoud@outlook.com>
2022-11-21 13:26:45 +0300

    Change Image filtering test values

--- tests/Image filtering/test.ts
+++ tests/Image filtering/test.ts
@@ -11,13 +11,13 @@ describe("Image filtering", () => {
     test.internationalTextualDataLength = 0;
     test.width = 32;
     test.height = 32;
-    test.bitDepth = 1;
-    test.color = 0;
+    test.bitDepth = 8;
+    test.color = 2;
     test.compressionMethod = 0;
     test.filterMethod = 0;
-    test.isInterlaced = 1;
-    test.gamaIntensity = 100000;
-    test.imageDataLength = 192;
+    test.isInterlaced = 0;
+    test.gamaIntensity = undefined;
+    test.imageDataLength = 3104;
 
     test.parsePng(path.join(__dirname, "images", "f01n2c08.png"), test);
   });
@@ -31,13 +31,13 @@ describe("Image filtering", () => {
     test.internationalTextualDataLength = 0;
     test.width = 32;
     test.height = 32;
-    test.bitDepth = 1;
+    test.bitDepth = 4;
     test.color = 0;
     test.compressionMethod = 0;
     test.filterMethod = 0;
-    test.isInterlaced = 1;
-    test.gamaIntensity = 100000;
-    test.imageDataLength = 192;
+    test.isInterlaced = 0;
+    test.gamaIntensity = undefined;
+    test.imageDataLength = 544;
 
     test.parsePng(path.join(__dirname, "images", "f99n0g04.png"), test);
   });
